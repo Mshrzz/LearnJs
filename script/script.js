04e8a311e678e2ad68c3514cd4f22177dfbcabce
Mshrzz <ohmymshrzz@gmail.com>
2021-03-10 23:07:12 +1000

    feat(script.js): added lesson29

--- script/script.js
+++ script/script.js
@@ -461,13 +461,13 @@ window.addEventListener('DOMContentLoaded', function(){
             statusMessage.textContent = loadMessage;
 
             const formData = new FormData(event.target);
-            let body = {};
 
-            formData.forEach((val, key) => {
-                body[key] = val;
-            });
-
-            postData(body)
+            postData(formData)
+                .then((response) => {
+                    if (response.status !== 200) {
+                        throw new Error('Network status is not 200');
+                    }
+                })
                 .then(() => {
                     let formInputs = event.target.querySelectorAll('input');
                 
@@ -490,25 +490,12 @@ window.addEventListener('DOMContentLoaded', function(){
 
         const postData = (body) => {
 
-            return new Promise((resolve, reject) => {
-                const request = new XMLHttpRequest();
-
-                request.addEventListener('readystatechange', () => {
-                    if (request.readyState !== 4) {
-                        return;
-                    }
-    
-                    if (request.status === 200) {
-                        resolve();
-                    } else {
-                        reject(request.status);
-                    }
-                });
-    
-                request.open('POST', './server.php');
-                request.setRequestHeader('Content-Type', 'application/json');
-    
-                request.send(JSON.stringify(body));
+            return fetch('./server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: body
             });
 
         };
